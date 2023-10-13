4d2088354dac39aefdaabc269d6d09eaefcda6f1
insiders404 <147330331+insiders404@users.noreply.github.com>
2023-10-13 06:14:17 +0530

    Add files via upload

---
+++ q1.php
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<students>
+    <student>
+        <name>John Doe</name>
+        <roll_number>101</roll_number>
+        <age>20</age>
+        <division>A</division>
+    </student>
+    <student>
+        <name>Jane Smith</name>
+        <roll_number>102</roll_number>
+        <age>21</age>
+        <division>B</division>
+    </student>
+    <student>
+        <name>Alice Johnson</name>
+        <roll_number>103</roll_number>
+        <age>19</age>
+        <division>C</division>
+    </student>
+</students>
